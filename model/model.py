323abc21506ea898f5a807661b08fe32a4cbd6c7
manhdung20112000 <manhdung20112000@gmail.com>
2021-05-17 15:06:44 +0700

    residual block & Unet with ResNet arch

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from collections import OrderedDict
 
-from utils.common import ConvBlock, DeconvBlock
+from utils.common import ConvBlock, DeconvBlock, ResidualBlock
 
 class UNet(nn.Module):
     def __init__(self, in_channels, n_classes):
@@ -24,8 +24,8 @@ class UNet(nn.Module):
         # Encoder Block 2
         self.encoder_2 = nn.Sequential(
             OrderedDict([
-                ('enconv2_1', ConvBlock(64, 128)),
-                ('enconv2_2', ConvBlock(128, 128)),
+                ('conv2_1', ConvBlock(64, 128)),
+                ('conv2_2', ConvBlock(128, 128)),
                 ('maxpool2', nn.MaxPool2d((2, 2)))
             ])
         )
@@ -33,8 +33,8 @@ class UNet(nn.Module):
         # Encoder Block 3
         self.encoder_3 = nn.Sequential(
             OrderedDict([
-                ('enconv3_1', ConvBlock(128, 256)),
-                ('enconv3_2', ConvBlock(256, 256)),
+                ('conv3_1', ConvBlock(128, 256)),
+                ('conv3_2', ConvBlock(256, 256)),
                 ('maxpool3', nn.MaxPool2d((2, 2)))
             ])
         )
@@ -42,17 +42,17 @@ class UNet(nn.Module):
         # Encoder Block 4
         self.encoder_4 = nn.Sequential(
             OrderedDict([
-                ('enconv4_1', ConvBlock(256, 512)),
-                ('enconv4_2', ConvBlock(512, 512)),
+                ('conv4_1', ConvBlock(256, 512)),
+                ('conv4_2', ConvBlock(512, 512)),
                 ('maxpool4', nn.MaxPool2d((2, 2)))
             ])
         )
 
         # Encoder Block 5
-        self.encoder_5 = nn.Sequential(
+        self.middle = nn.Sequential(
             OrderedDict([
-                ('enconv5_1', ConvBlock(512, 1024)),
-                ('enconv5_2', ConvBlock(1024, 1024))
+                ('conv5_1', ConvBlock(512, 1024)),
+                ('conv5_2', ConvBlock(1024, 1024))
             ])
         )
 
@@ -61,7 +61,7 @@ class UNet(nn.Module):
         # Decoder Block 4 
         self.decoder_4 = nn.Sequential(
             OrderedDict([
-                ('deconv4_1', ConvBlock(1024, 512)),
+                ('deconv4_1', ConvBlock(512, 512)),
                 ('deconv4_2', ConvBlock(512, 512))
             ])
         )
@@ -71,7 +71,7 @@ class UNet(nn.Module):
         # Decoder Block 3
         self.decoder_3 = nn.Sequential(
             OrderedDict([
-                ('deconv3_1', ConvBlock(512, 256)),
+                ('deconv3_1', ConvBlock(256, 256)),
                 ('deconv3_2', ConvBlock(256, 256))
             ])
         )
@@ -81,7 +81,7 @@ class UNet(nn.Module):
         # Decoder Block 2
         self.decoder_2 = nn.Sequential(
             OrderedDict([
-                ('deconv2_1', ConvBlock(256, 128)),
+                ('deconv2_1', ConvBlock(128, 128)),
                 ('deconv2_2', ConvBlock(128, 128))
             ])
         )
@@ -91,7 +91,7 @@ class UNet(nn.Module):
         # Decoder Block 1
         self.decoder_1 = nn.Sequential(
             OrderedDict([
-                ('deconv1_1', ConvBlock(128, 64)),
+                ('deconv1_1', ConvBlock(64, 64)),
                 ('deconv1_2', ConvBlock(64, 64)),
                 ('deconv1_3', ConvBlock(64, self.n_classes, (1,1), activation=False)),
             ])
@@ -127,4 +127,134 @@ class UNet(nn.Module):
 
         return x
 
+class UNet_ResNet(nn.Module):
+    def __init__(self, in_channels, n_classes, dropout=0.5):
+        super(UNet_ResNet).__init__()
         
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        self.dropout = dropout
+
+        self.encoder_1 = nn.Sequential(
+            OrderedDict([
+                ('conv1', nn.Conv2d(in_channels, 64, (3,3), padding_mode='same')),
+                ('res1_1', ResidualBlock(64)),
+                ('res1_2', ResidualBlock(64, batch_activation=True)),
+                ('maxpool1', nn.MaxPool2d((2,2))),
+                ('dropout1', nn.Dropout2d(self.dropout/2)),
+            ])
+        )
+
+        self.encoder_2 = nn.Sequential(
+            OrderedDict([
+                ('conv2', nn.Conv2d(in_channels, 128, (3,3), padding_mode='same')),
+                ('res2_1', ResidualBlock(128)),
+                ('res2_2', ResidualBlock(128, batch_activation=True)),
+                ('maxpool2', nn.MaxPool2d((2,2))),
+                ('dropout2', nn.Dropout2d(self.dropout)),
+            ])
+        )
+
+        self.encoder_3 = nn.Sequential(
+            OrderedDict([
+                ('conv3', nn.Conv2d(in_channels, 256, (3,3), padding_mode='same')),
+                ('res3_1', ResidualBlock(256)),
+                ('res3_2', ResidualBlock(256, batch_activation=True)),
+                ('maxpool3', nn.MaxPool2d((2,2))),
+                ('dropout3', nn.Dropout2d(self.dropout)),
+            ])
+        )
+        
+        self.encoder_4 = nn.Sequential(
+            OrderedDict([
+                ('conv4', nn.Conv2d(in_channels, 512, (3,3), padding_mode='same')),
+                ('res4_1', ResidualBlock(512)),
+                ('res4_2', ResidualBlock(512, batch_activation=True)),
+                ('maxpool4', nn.MaxPool2d((2,2))),
+                ('dropout4', nn.Dropout2d(self.dropout)),
+            ])
+        )
+
+        self.middle = nn.Sequential(
+            OrderedDict([
+                ('conv5', nn.Conv2d(in_channels, 1024, (3,3), padding_mode='same')),
+                ('res5_1', ResidualBlock(1024)),
+                ('res5_2', ResidualBlock(1024, batch_activation=True)),
+            ])
+        )
+        
+        self.upconv4 = nn.Sequential(OrderedDict([('upconv4', DeconvBlock(1024))]))
+
+        self.deconder_4 = nn.Sequential(
+            OrderedDict([
+                ('dropout4', nn.Dropout2d(self.dropout)),
+                ('deconv4', nn.Conv2d(512, 512, (3,3))),
+                ('res4_1', ResidualBlock(512)),
+                ('res4_2', ResidualBlock(512, batch_activation=True)),
+            ])
+        )
+
+        self.upconv3 = nn.Sequential(OrderedDict([('upconv3', DeconvBlock(512))]))
+
+        self.deconder_3 = nn.Sequential(
+            OrderedDict([
+                ('dropout3', nn.Dropout2d(self.dropout)),
+                ('deconv3', nn.Conv2d(256, 256, (3,3))),
+                ('res3_1', ResidualBlock(256)),
+                ('res3_2', ResidualBlock(256, batch_activation=True)),
+            ])
+        )
+
+        self.upconv2= nn.Sequential(OrderedDict([('upconv2', DeconvBlock(256))]))
+
+        self.deconder_2 = nn.Sequential(
+            OrderedDict([
+                ('dropout2', nn.Dropout2d(self.dropout)),
+                ('deconv2', nn.Conv2d(128, 128, (3,3))),
+                ('res2_1', ResidualBlock(128)),
+                ('res2_2', ResidualBlock(128, batch_activation=True)),
+            ])
+        )
+
+        self.upconv1 = nn.Sequential(OrderedDict([('upconv1', DeconvBlock(128))]))
+
+        self.deconder_1 = nn.Sequential(
+            OrderedDict([
+                ('upconv1', DeconvBlock(128)),
+                ('dropout1', nn.Dropout2d(self.dropout)),
+                ('deconv1_1', nn.Conv2d(64, 64, (3,3))),
+                ('res1_1', ResidualBlock(64)),
+                ('res1_2', ResidualBlock(64, batch_activation=True)),
+                ('deconv1_2', ConvBlock(64, self.n_classes, kernel=(1,1), activation=False))
+            ])
+        )
+
+    def forward(self, x):
+        # Encoder
+        # 1-64
+        encoder_1 = self.encoder_1(x)
+        # 64-128
+        encoder_2 = self.encoder_2(encoder_1)
+        # 128-256
+        encoder_3 = self.encoder_2(encoder_2)
+        # 256-512
+        encoder_4 = self.encoder_2(encoder_3)
+        
+        # 512-1024
+        x = self.encoder_2(encoder_4)
+
+        # Decoder
+        # 1024-512
+        x = self.upconv4(x, encoder_4)
+        x = self.decoder_4(x)
+        # 512-256
+        x = self.upconv3(x, encoder_3)
+        x = self.decoder_3(x)
+        # 256-128
+        x = self.upconv2(x, encoder_2)
+        x = self.decoder_2(x)
+        #128-64-2
+        x = self.upconv1(x, encoder_1)
+        x = self.decoder_1(x)
+
+        return x
--- utils/common.py
+++ utils/common.py
@@ -1,31 +1,52 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.modules.conv import Conv2d
+
+class BatchActivate(nn.Module):
+    def __init__(self, in_channels):
+        super(BatchActivate).__init__()
+        self.norm = nn.BatchNorm2d(in_channels)
+
+    def forward(self, x):
+        x = F.relu(self.norm(x))
+        return x
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel=(3,3), activation=True):
         super(ConvBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
-                        kernel_size=kernel)
-        self.norm = nn.BatchNorm2d(out_channels)
-        self.activation = activation
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel)
+        self.batchlayer = BatchActivate(out_channels)
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.norm(x)
         if self.activation:
-            x = F.relu(x)
+            self.batchlayer(x)
         return x
 
 class DeconvBlock(nn.Module):
     def __init__(self, in_channels, kernel=(2,2)):
         super(DeconvBlock, self).__init__()
-        self.deconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels,
+        self.deconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels/2,
                                         kernel_size=kernel)
 
     def forward(self, x1, x2):
         x_temp = self.deconv(x1)
         x =  torch.cat([x_temp, x2], dim=1)
         return x
-        
-        
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, kernel_size=(3,3), batch_activation=False):
+        super(ResidualBlock).__init__()
+        self.batch_activation = batch_activation
+        self.norm_relu = BatchActivate(in_channels)
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size)
+
+    def forward(self, input):
+        x = self.norm_relu(input)
+        x = self.conv(x)
+        x = F.relu(self.conv(x))
+        x = torch.cat([x, input], dim=1)
+        if self.batch_activation:
+            x = self.norm(x)
+        return x
